5fcb970a6bdf6ec11b20c3d276b2a0aa1d1ecbce
Vincent Liu <vince.liu38@gmail.com>
2017-06-25 11:15:29 -0400

    filter box implemented

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ angular.module('app', ['ngMaterial'])
 
 .controller('mainCtrl', function($scope, $http) {
 
+  {
+    $http({
+      method: 'GET',
+      url: 'https://biscuitandchai.com/whiskeyandvodka/api/'
+    }).then(function successCallback(response) {
+      $scope.cocktails = response.data;
+      console.log(response.data)
+    }).then(function errorCallback(response) {
+      console.log(response);
+    })
+  }
 })
